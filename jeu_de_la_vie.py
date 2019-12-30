220c86d1a38aa6a95f591f371df7e26e7f04df53
Pascal <quach.pascal@gmail.com>
2019-12-30 19:50:56 +0100

    Initialisation de la grille
    # Grille
    - Taille ajustable 
    - Taille du bloc ajusté à la taille de la grille automatiquement
    - Pourcentage de vie pris en compte
    # To Do 
    - Bloquer les sliders taille grille et pourcentage vie lorsque le jeu est en cours

--- jeu_de_la_vie.py
+++ jeu_de_la_vie.py
@@ -1,73 +1,166 @@
 from tkinter import *   ## notice lowercase 't' in tkinter here
 from tkinter.messagebox import *
 from time import *
-from random import randint
-
+from random import *
+from math import *
 # ------------- PARAMETRES ---------------
 
 ## -- Fenêtre principale -- 
 couleur_cellule = 'red'
 couleur_fond = 'white'
-hauteur_fenetre = 600
-largeur_fenetre = 600
+hauteur_fenetre = 834
+largeur_fenetre = 834
 statut = False # Lancer le jeu ou le mettre en pause
 
 ## -- Initialisation --
 ## Taille de la grille
-taille_grille = 30
-taille_bloc = taille_grille/6
-## Pourcentage de vie
-pourcentage_vie = 20
+taille_grille = 5
+taille_bloc = hauteur_fenetre/taille_grille
+pourcentage_vie_actuel = 0
 
-## -- Temps réel -- 
+## Tableau
+tableau = [[bool(0) for j in range(taille_grille)] for i in range(taille_grille)]
+copie_tableau = [[bool(0) for j in range(taille_grille)] for i in range(taille_grille)]
+
+## Pourcentage de vie
+pourcentage_vie = 0
+
 ## Vitesse 
-vitesse_animation = 5
+vitesse_animation = 1
 
 # -------------  FONCTIONS ---------------
 
 ## -- Initialiser la grille --
 ## -- Lancer le jeu --
 def lancer_jeu():
+    global statut
     statut = True
-    print("Le jeu s'est lancé\n")
+    print("Le jeu s'est lancé; statut = %d" % statut)
 ## -- Arrêter le jeu --
 def arreter_jeu():
+    global statut
     statut = False
-    print("Le jeu s'est arrêté\n")
+    print("Le jeu s'est arrêté; statut = %d" % statut)
+
+## -- Initialiser le tableau grille[][] avec des valeurs random --     
+def initialiser_tableau(tableau):    
+    global pourcentage_vie
+    global pourcentage_vie_actuel
+    global taille_grille
+    nombre_de_blocs = taille_grille*taille_grille
     
+    pourcentage_vie_actuel = 0
+    for i in range(0,taille_grille):
+        for j in range(0,taille_grille):
+            tableau[i][j] = False
+    
+    while (pourcentage_vie_actuel <= pourcentage_vie and pourcentage_vie != float(0)):
+        x_random = randint(0, taille_grille-1)
+        #print("<---- x rand : %d --->" % x_random)
+        y_random = randint(0, taille_grille-1)
+        #print("<---- y rand : %d --->" % y_random)
+        tableau[x_random][y_random] = True
+        pourcentage_vie_actuel += float(1/(nombre_de_blocs))
+        #print("<---- pourcentage actuel : %f --->" % pourcentage_vie_actuel)
+    
+    print("La grille est initialisée.\n")
+
+
+## -- Dessiner la grille vide --
+def dessiner_grille(tableau):
+    global taille_grille
+    global taille_bloc
+    global couleur_cellule
+    global couleur_fond
+    
+    # Effacer la grille précédente
+    canevas.delete("all")
+        
+    # Dessiner la nouvelle grille
+    position_x = 0
+    position_y = 0
+    for i in range(taille_grille):
+        position_x = 0
+        for j in range(taille_grille):
+            tag = str(position_x) + "-" + str(position_y)
+            if (tableau[i][j] == True):
+                canevas.create_rectangle(position_x, position_y, position_x + taille_bloc, position_y + taille_bloc, fill=couleur_cellule, tags=("Cellule vivante", tag))
+            else:
+                canevas.create_rectangle(position_x, position_y, position_x + taille_bloc, position_y + taille_bloc, fill=couleur_fond, tags=("Cellule morte", tag))
+            position_x += taille_bloc
+        position_y += taille_bloc    
+    
+    # Debugging
+    print(canevas.find_all())
+    
+## -- Initialiser le jeu --         
 def initialiser_jeu():
+    # Appeler initialiser_grille(grille, pourcentage_vie)
+    # Appeler dessiner_grille(canevas)
+    initialiser_tableau(tableau)
+    dessiner_grille(tableau)
+    print("Le jeu est initialisé.\n")
     
+
     
+## -- Compter le nombre de voisins d'une case grille[i][j] --         
+def nombre_de_voisins(i, j, grille):
+    nbr_de_voisins = 0
+    
+    print("Nombre de voisins de (%d, %d) est %d" % i, j, nbr_de_voisins)
+
+## -- Mise à jour grille --             
+def mise_a_jour_grille(grille, copie_grille):
     
     
     print("Le jeu est initialisé\n")
 
+## -- Mise à jour des Sliders -- 
 def slider_taille_grille(val):
-    taille_grille = val
-    #print("Slider_val =",val,"\nTaille grille = ", taille_grille, "\nPourcentage_vie =", pourcentage_vie, "\nVitesse = ", vitesse_animation,"\n")
+    global taille_grille
+    global taille_bloc
+    global pourcentage_vie
+    global vitesse_animation
+    global tableau
+    taille_grille = int(val)
+    taille_bloc = hauteur_fenetre/taille_grille
+    print("Slider_val =",val,"\nTaille grille = ", taille_grille, "\nPourcentage_vie =", pourcentage_vie, "\nVitesse = ", vitesse_animation,"\n")
+    tableau = [[0 for j in range(taille_grille)] for i in range(taille_grille)]
+
     
 def slider_pourcentage_vie(val):
-    pourcentage_vie = val
-    #print("Slider_val =",val,"\nTaille grille = ", taille_grille, "\nPourcentage_vie =", pourcentage_vie, "\nVitesse = ", vitesse_animation,"\n")
+    global taille_grille
+    global pourcentage_vie
+    global vitesse_animation
+    pourcentage_vie = float(val)
+    print("Slider_val =",val,"\nTaille grille = ", taille_grille, "\nPourcentage_vie =", pourcentage_vie, "\nVitesse = ", vitesse_animation,"\n")
     
 def slider_vitesse_animation(val):
-    vitesse_animation = val
-    #print("Slider_val =",val,"\nTaille grille = ", taille_grille, "\nPourcentage_vie =", pourcentage_vie, "\nVitesse = ", vitesse_animation,"\n")
+    global taille_grille
+    global pourcentage_vie
+    global vitesse_animation 
+    vitesse_animation = int(val)
+    print("Slider_val =",val,"\nTaille grille = ", taille_grille, "\nPourcentage_vie =", pourcentage_vie, "\nVitesse = ", vitesse_animation,"\n")
     
 
 # ----------- INITIALISATION -------------
 #grille = # On définit la variable grille
 #sauvegarde_grille =  # On définit de même sa sauvegarde
 
+# Configuration
 root = Tk() # Fenêtre principale Tkinter
 root.title("SR01 - Devoir 3 - Jeu de la vie")
 root.resizable(False, False)
+
 # Barre menu
 menu_cote = Frame(root, width=hauteur_fenetre*0.3, height=hauteur_fenetre, bg='#f0f0f0', relief='flat', borderwidth=2)
 menu_cote.pack(expand=False, fill='both', side='right', anchor='nw')
 # Fenêtre grille    
-canevas = Frame(root, width=largeur_fenetre, height=hauteur_fenetre, bg='white')
-canevas.pack(expand=False, fill='both', side='left')
+grille = Frame(root, width=largeur_fenetre, height=hauteur_fenetre, bg='white')
+grille.pack(expand=False, fill='both', side='left')
+# Canevas
+canevas = Canvas(grille, width=largeur_fenetre, height=hauteur_fenetre, bg='white')
+canevas.pack(fill=BOTH, expand=1)
     
 # -----------  MENU TKINTER WIDGETS --------------
 
@@ -82,24 +175,21 @@ bouton_arreter.pack(side='top')
 bouton_init = Button(menu_cote, text='Initialiser', relief='solid', fg='blue', width=20, command=initialiser_jeu)
 bouton_init.pack(side='top')
 
-
 ## Quitter ## Bouton
 bouton_init = Button(menu_cote, text='Quitter', relief='solid', fg='blue', width=20, command=root.destroy)
 bouton_init.pack(side='bottom')
 
 ## Vitesse ## -- Paramètres init --
-bouton_vitesse = Scale(menu_cote, orient='horizontal', from_=1, to=100, length=120, variable=vitesse_animation, bg='#f0f0f0', relief='flat', highlightthickness=0, label='Vitesse', fg='blue', command=slider_vitesse_animation)
+bouton_vitesse = Scale(menu_cote, orient='horizontal', from_=1, to=100, length=180, variable=vitesse_animation, bg='#f0f0f0', relief='flat', highlightthickness=0, label='Vitesse', fg='blue', command=slider_vitesse_animation)
 bouton_vitesse.pack(side='bottom')
 
 ## Pourcentage de vie ## -- Paramètres real time -- 
-bouton_pourcentage_vie = Scale(menu_cote, orient='horizontal', from_=0, to=100, length=120, variable=pourcentage_vie, bg='#f0f0f0', relief='flat', highlightthickness=0, label='% de Vie', fg='blue', bd='0', command=slider_pourcentage_vie)
+bouton_pourcentage_vie = Scale(menu_cote, orient='horizontal', from_=0, to=1, length=180,  variable=pourcentage_vie, resolution=0.001, bg='#f0f0f0', relief='flat', highlightthickness=0, label='% de Vie', fg='blue', bd='0', command=slider_pourcentage_vie)
 bouton_pourcentage_vie.pack(side='bottom')
 
 ## Taille de la grille ## -- Paramètres init --
-bouton_taille_grille = Scale(menu_cote, orient='horizontal', from_=1, to=100, length=120, variable=taille_grille, bg='#f0f0f0', relief='flat', highlightthickness=0, label='Taille de la grille', fg='blue', command=slider_taille_grille)
+bouton_taille_grille = Scale(menu_cote, orient='horizontal', from_=5, to=500, length=180, variable=taille_grille, bg='#f0f0f0', relief='flat', highlightthickness=0, label='Taille de la grille', fg='blue', command=slider_taille_grille)
 bouton_taille_grille.pack(side='bottom')
 
 
 root.mainloop() # Appel boucle
-
-
