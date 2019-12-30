a97ea66bd112f0f8324ba4d404b48c0cc583ab45
Pascal <quach.pascal@gmail.com>
2019-12-30 15:05:40 +0100

    Side bar
    # Boutons implémentés
    # Next : lié à une variable

--- jeu_de_la_vie.py
+++ jeu_de_la_vie.py
@@ -27,21 +27,6 @@ vitesse_animation = 5
 
 
 
-
-# -----------  MENU TKINTER WIDGETS --------------
-
-## -- Boutons --
-## Lancer
-## Arreter
-## Initialiser
-
-## -- Paramètres init --
-## Taille de la grille
-## Pourcentage de vie
-
-## -- Paramètres real time -- 
-## Vitesse
-
 # ----------- INITIALISATION -------------
 #grille = # On définit la variable grille
 #sauvegarde_grille =  # On définit de même sa sauvegarde
@@ -50,13 +35,41 @@ root = Tk() # Fenêtre principale Tkinter
 root.title("SR01 - Devoir 3 - Jeu de la vie")
 root.resizable(False, False)
 # Barre menu
-sidebar = Frame(root, width=hauteur_fenetre*0.3, height=hauteur_fenetre, bg='black', relief='sunken', borderwidth=2)
-sidebar.pack(expand=False, fill='both', side='right', anchor='nw')
-
+menu_cote = Frame(root, width=hauteur_fenetre*0.3, height=hauteur_fenetre, bg='#CCC', relief='sunken', borderwidth=2)
+menu_cote.pack(expand=False, fill='both', side='right', anchor='nw')
 # Fenêtre grille    
 canevas = Frame(root, width=largeur_fenetre, height=hauteur_fenetre, bg='white')
 canevas.pack(expand=False, fill='both', side='left')
 
 
 
+# -----------  MENU TKINTER WIDGETS --------------
+
+## -- Boutons --
+## Lancer 
+bouton_lancer = Button(menu_cote, text='Lancer', fg='blue', width=20)
+bouton_lancer.pack(side='top')
+## Arreter
+bouton_arreter = Button(menu_cote, text='Arreter', fg='blue', width=20)
+bouton_arreter.pack(side='top')
+## Initialiser
+bouton_init = Button(menu_cote, text='Initialiser', fg='blue', width=20)
+bouton_init.pack(side='top')
+
+## -- Paramètres real time -- 
+## Pourcentage de vie
+bouton_taille_grille = Scale(menu_cote, orient='horizontal', from_=0, to=100, length=120, label='% de Vie', fg='blue')
+bouton_taille_grille.pack(side='bottom')
+
+## -- Paramètres init --
+## Vitesse
+bouton_taille_grille = Scale(menu_cote, orient='horizontal', from_=1, to=100, length=120, label='Vitesse', fg='blue')
+bouton_taille_grille.pack(side='bottom')
+## Taille de la grille
+bouton_taille_grille = Scale(menu_cote, orient='horizontal', from_=1, to=100, length=120, label='Taille de la grille', fg='blue')
+bouton_taille_grille.pack(side='bottom')
+
+
 root.mainloop() # Appel boucle
+
+
